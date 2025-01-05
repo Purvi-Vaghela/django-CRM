9fbd88c844eeeea71c9d2904fb9c843eb6150630
PurviVaghela <purvi09v@gmail.com>
2025-01-05 15:39:19 +0530

    final commit

--- website/models.py
+++ website/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	first_name = models.CharField(max_length=50)
+	last_name =  models.CharField(max_length=50)
+	email =  models.CharField(max_length=100)
+	phone = models.CharField(max_length=15)
+	address =  models.CharField(max_length=100)
+	city =  models.CharField(max_length=50)
+	state =  models.CharField(max_length=50)
+	zipcode =  models.CharField(max_length=20)
+
+	def __str__(self):
+		return(f"{self.first_name} {self.last_name}")   
+
+
+# The __str__ method is a special method in Python that overrides the default string representation of an object.
+
+# Django Admin: When you view a list of Record instances in the Django admin interface, 
+# the __str__ representation is displayed instead of the default object name.
